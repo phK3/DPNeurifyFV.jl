c75f03732bd7cc32eba43cddae6b7cc432439463
philipp <philipp.kern@kit.edu>
2022-04-26 15:10:05 +0200

    add BaB optimization

--- src/DPNeurifyFV.jl
+++ src/DPNeurifyFV.jl
@@ -1,7 +1,9 @@
 module DPNeurifyFV
 
-using LazySets, NeuralVerification, Parameters, LinearAlgebra
-import NeuralVerification: Layer, Network, AbstractNetwork, ActivationFunction, ReLU, Id, n_nodes, affine_map, interval_map, relaxed_relu_gradient
+using LazySets, NeuralVerification, Parameters, LinearAlgebra, DataStructures, NeuralPriorityOptimizer
+using NeuralVerification: TOL, Layer, Network, AbstractNetwork, ActivationFunction, ReLU, Id, n_nodes, relaxed_relu_gradient, compute_output
+import NeuralVerification: affine_map, interval_map
+import NeuralPriorityOptimizer: split_hyperrectangle, split_largest_interval
 const NV = NeuralVerification
 
 # redefinitions of function defined in NeuralVerification.jl
@@ -11,11 +13,15 @@ include("util.jl")
 include("symbolic_interval_fv_heur.jl")
 include("fresh_var_heuristic.jl")
 include("dp_neurify_fv.jl")
+include("optimization_bab.jl")
 
 export 
     NetworkNegPosIdx,
     DPNFV,
-    init_symbolic_interval_fvheur
+    init_symbolic_interval_fvheur,
+    optimize_linear_deep_poly,
+    contained_within_polytope_deep_poly,
+    reaches_polytope_deep_poly
     
 
 end # module
