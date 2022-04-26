d391a7384606acd763dfce9de79c7e57dc7b8248
philipp <philipp.kern@kit.edu>
2022-04-26 12:33:39 +0200

    include heuristics and LinearAlgebra

--- src/DPNeurifyFV.jl
+++ src/DPNeurifyFV.jl
@@ -1,7 +1,7 @@
 module DPNeurifyFV
 
-using LazySets, NeuralVerification, Parameters
-import NeuralVerification: Layer, Network, AbstractNetwork, ActivationFunction, ReLU, Id
+using LazySets, NeuralVerification, Parameters, LinearAlgebra
+import NeuralVerification: Layer, Network, AbstractNetwork, ActivationFunction, ReLU, Id, n_nodes, affine_map, interval_map, relaxed_relu_gradient
 const NV = NeuralVerification
 
 # redefinitions of function defined in NeuralVerification.jl
@@ -9,6 +9,7 @@ include("overwrite_neural_verification.jl")
 include("network_neg_pos_idx.jl")
 include("util.jl")
 include("symbolic_interval_fv_heur.jl")
+include("fresh_var_heuristic.jl")
 include("dp_neurify_fv.jl")
 
 export 
