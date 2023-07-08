82ca98b026af5f143875f33ad73d49bc29c18372
phK3 <ufedz@student.kit.edu>
2023-07-08 08:21:26 +0200

    try to force precompilation

--- src/DPNeurifyFV.jl
+++ src/DPNeurifyFV.jl
@@ -1,7 +1,7 @@
 module DPNeurifyFV
 
 using LazySets, NeuralVerification, Parameters, LinearAlgebra, DataStructures, NeuralPriorityOptimizer, CSV, 
-        OnnxReader, VnnlibParser, Flux, VNNLib
+        OnnxReader, VnnlibParser, Flux, VNNLib, PyVnnlib
 using NeuralVerification: TOL, Layer, Network, AbstractNetwork, ActivationFunction, ReLU, Id, n_nodes, relaxed_relu_gradient, compute_output
 import NeuralVerification: affine_map, interval_map
 import NeuralPriorityOptimizer: split_hyperrectangle, split_largest_interval, split_multiple_times
@@ -25,6 +25,14 @@ include("computational_graph/computational_nodes.jl")
 include("computational_graph/symbolic_interval_graph.jl")
 include("computational_graph/dp_neurify_fv_graph.jl")
 include("computational_graph/onnx_constructors.jl")
+include("computational_graph/cg_optimization_bab.jl")
+
+# does it get precompiled?
+params = PriorityOptimizerParameters(max_steps=3, print_frequency=1, stop_frequency=1, verbosity=2)
+solver = DPNFV(method=:DeepPolyRelax)
+onnx_file = string(@__DIR__, "/../networks/precompile_nns/ACASXU_run2a_1_1_batch_2000.onnx")
+vnnlib_file = string(@__DIR__,"/../networks/precompile_nns/prop_1.vnnlib")
+verify_vnnlib(solver, onnx_file, vnnlib_file, params, printing=true);
 
 export 
     NetworkNegPosIdx,
