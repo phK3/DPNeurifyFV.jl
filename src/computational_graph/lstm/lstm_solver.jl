48de1e8e075d87f3c9a9fa443a2e5c490b616396
phK3 <ufedz@student.kit.edu>
2023-08-24 16:29:44 +0200

    use stored intermediate bounds

--- src/computational_graph/lstm/lstm_solver.jl
+++ src/computational_graph/lstm/lstm_solver.jl
@@ -25,38 +25,11 @@ end
 
 function forward_node(solver::LSTMSolver, L::Relu, sz::SplitZonotope)
     z = sz.z
-    H = overapproximate(z, Hyperrectangle)
-    l = low(H)
-    u = high(H)
+    l, u = get_bounds!(sz, L.name)
 
     # adding split constraints
     if L.name in keys(sz.splits)
-        split_idxs, l_split, u_split = unzip(sz.splits[L.name])
-        split_idxs = _vec(split_idxs)
-        l_split = _vec(l_split)
-        u_split = _vec(u_split)
-
-        # we need those splits, others already hold by bounds from input zonotope
-        lmask = l[split_idxs] .< l_split
-        umask = u[split_idxs] .> u_split
-
-        # x ≥ l ↔ -x ≤ -l 
-        A_l = .- z.generators[split_idxs[lmask], :]
-        b_l = .- l_split[lmask]
-
-        A_u = z.generators[split_idxs[umask], :]
-        b_u = u_split[umask]
-
-        A = [A_l; A_u]
-        b = [b_l; b_u]
-
-        # TODO: right number of generators?
-        Â, b̂ = get_constraints_matrix(sz, A, b)
-
-
-        # now can adjust bounds for propagation
-        l[split_idxs] .= max.(l[split_idxs], l_split)
-        u[split_idxs] .= min.(u[split_idxs], u_split)
+        l, u, Â, b̂ = get_split_bounds(sz, sz.splits[L.name], l, u)
     else
         Â = sz.split_A
         b̂ = sz.split_b
@@ -73,7 +46,7 @@ function forward_node(solver::LSTMSolver, L::Relu, sz::SplitZonotope)
 
     # register new generators in generator_map
 
-    sẑ = SplitZonotope(ẑ, sz.splits, sz.generator_map, Â, b̂, sz.shape)
+    sẑ = SplitZonotope(ẑ, sz.splits, sz.bounds, sz.generator_map, Â, b̂, sz.shape)
     push!(sẑ.generator_map, [(L.name, i) for i in (1:dim(z))[crossing]]...)
     return sẑ
 end
@@ -82,5 +55,5 @@ end
 function forward_node(solver::LSTMSolver, L::Flatten, sz::SplitZonotope{N}) where N <: Number
     c = reshape(sz.z.center, sz.shape)
     ĉ = reshape(c, :, size(c)[end])
-    return SplitZonotope(sz.z, sz.splits, sz.generator_map, sz.split_A, sz.split_b, size(ĉ))
+    return SplitZonotope(sz.z, sz.splits, sz.bounds, sz.generator_map, sz.split_A, sz.split_b, size(ĉ))
 end
