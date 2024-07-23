4df0cc30bfb3993d39e40a42fe1b1fa01b6d569f
phK3 <ufedz@student.kit.edu>
2024-07-23 11:28:43 +0200

    add maxvars to precompile template

--- vnncomp_scripts/precompile_template.jl
+++ vnncomp_scripts/precompile_template.jl
@@ -2,7 +2,7 @@ using DPNeurifyFV, PyVnnlib, LazySets, Flux, NeuralVerification, NeuralPriorityO
 const DP = DPNeurifyFV
 
 params = DP.PriorityOptimizerParameters(max_steps=3, print_frequency=1, stop_frequency=1, verbosity=2)
-solver = DPNFV(method=:DeepPolyRelax)
+solver = DPNFV(method=:DeepPolyRelax, max_vars=10)
 onnx_file = string(@__DIR__, "/../networks/precompile_nns/ACASXU_run2a_1_1_batch_2000.onnx")
 vnnlib_file = string(@__DIR__,"/../networks/precompile_nns/prop_1.vnnlib")
 DP.verify_vnnlib(solver, onnx_file, vnnlib_file, params, printing=true);
